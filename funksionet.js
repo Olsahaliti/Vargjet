f0ca45bf59364527fc9a7a6540f66429a1933f84
Olsahaliti <olsahaliti931@gmail.com>
2025-01-21 10:42:13 -0800

    punova

---
+++ funksionet.js
@@ -0,0 +1,87 @@
+//3 lloje te funksioneve
+//2 mnyra per mu shkrujt funksionet
+
+// let SayHello = () => { 
+//     console.log("Hello");
+// }
+// function Ec() {
+//     console.log("jom tu ec");
+// }
+// //arrow function
+// let ec2 = () => { 
+//     console.log("jom tu ec");
+// };
+// SayHello();
+// SayHello();
+// Ec();
+// ec2();
+
+// 3 llojet e funksioneve:
+//1.Funksionet pa parametra dhe pa return
+// function SayHello() { 
+// console.log("Hello")
+// }
+//SayHello();
+//Sayhello();
+// 2. Funksionet qe kane parametra
+//function SayHello (emri) { 
+// //console.log("Hello " + emri )
+//}
+// SayHello("Mirjeta")
+// SayHello("12312312123")
+// // 3.Funksionet qe kane return 
+//SazHello("m")
+//let rezultati = prompt("Shkruaj nje emer")
+//SayHello(rezultati);
+
+// function CiftApoTek(njeNumer) { 
+//     if (njeNumer % 2 == 0) { 
+//         return "cift";
+//     } else {
+//         return "tek"
+//     }
+// }
+
+// console.log(CiftApoTek(15));
+// let rezultati = CiftApoTek(2);
+// CiftApoTek(10);
+// CiftApoTek(3);
+
+// let vitiLindjes = prompt("shkruaj vitin e lindjes");
+
+// function kalkuloMshen (vlera) {
+//     let rezultati = 2025 - vlera;
+//     return rezultati;//24
+// }
+
+//  let mosha = kalkuloMoshen(vitiLindjes);
+//  console.log("variabla mosha" + mosha);
+//  console.log(kalkuloMoshen(vitiLindjes))
+//  //simple calculator
+//  //repat a word
+
+//  //gjeje numrin me te madh
+ function numriMeIMadh(nr1,nr2){
+    if(nr1 > nr2) { 
+        return nr1;
+    }else if(nr1 = nr2) { 
+        return "numrat jane te barabarte";
+    }else{
+        return numri2;
+     }
+    }
+    console.log(numriMeIMadh(3,4))
+
+     function repeatWord(fjala,numriPerseritjeve) {
+        if (fjala == "scantech") { 
+            console.log("nuk e printojme kete fjale");
+            return;
+        }
+        for(let i = 1;i<=numriPerseritjeve;i++) {
+            console.log(fjala);
+        }
+     }
+     repeatWord("mirjeta", 15);
+     repeatWord("mirjeta1234",10);
+     repeatWord("a",100);
+     repeatWord(12345,4);
